756e3dbbc59c62e4a50706730731441a3529082a
Mark Gerard <mark.musoba@bleacherreport.com>
2020-05-01 17:46:41 +0300

    Add initial project structure.

---
+++ src/main.cljs
@@ -0,0 +1,18 @@
+(ns main
+  (:require [reagent.core :as r]
+            [cljs.core.async :refer (chan put! <! go go-loop timeout)]))
+
+(defn main-component []
+  [:div
+   [:h1 "This is a comment"]])
+
+(defn mount [c]
+  (r/render-component [c] (.getElementById js/document "app")))
+
+(defn reload! []
+  (mount main-component)
+  (print "Hello reload!"))
+
+(defn main! []
+  (mount main-component)
+  (print "Hello Main"))
