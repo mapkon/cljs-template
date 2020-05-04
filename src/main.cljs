f7e21da0587685f5bfd7e8704f1c3ea60d6d27c1
mapkon <kata.mapkon@gmail.com>
2020-05-04 17:09:00 +0300

    overeager refactoring broke the build (variation on the cat ate the source code).

--- src/main.cljs
+++ src/main.cljs
@@ -7,7 +7,7 @@
    [:h1 "This is a comment"]])
 
 (defn mount [c]
-  (r/render-component [c] (.getElementById js/document "arborescence")))
+  (r/render-component [c] (.getElementById js/document "app")))
 
 (defn reload! []
   (mount main-component)
